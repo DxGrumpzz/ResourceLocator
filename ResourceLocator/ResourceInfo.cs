2244d16fc19422f27fe5ddd0f0304d521683cb17
Josef Lintz <yosi1.lints@gmail.com>
2019-06-26 12:09:21 +0200

    + Added ResourceInfo. A class that contains info about an embedded resource

---
+++ ResourceLocator/ResourceInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace ResourceLocator
+{
+    using System.IO;
+
+    /// <summary>
+    /// A class that contains information about an embedded resource
+    /// </summary>
+    public class ResourceInfo
+    {
+
+        public string ResourceName { get; set; }
+        public Stream ResourceStream { get; set; }
+
+    }
+}
