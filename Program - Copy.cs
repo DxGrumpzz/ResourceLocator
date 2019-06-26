a9aecffcd10ec3f4a63e1aeb6169e760dfcd2d56
Josef Lintz <yosi1.lints@gmail.com>
2019-06-26 11:12:16 +0200

    + Added project files

---
+++ Program - Copy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+
+namespace ResourceLocator
+{
+    /// <summary>
+    /// A resource locator that finds embedded resources in an application
+    /// </summary>
+    public class ResourceLocator
+    {
+        private Assembly _currentAssembly;
+
+        /// <summary>
+        /// Default constructor creates an assembly from the calling function
+        /// </summary>
+        public ResourceLocator() : this(Assembly.GetCallingAssembly())
+        {
+        }
+
+        public ResourceLocator(Assembly assembly)
+        {
+            _currentAssembly = assembly;
+        }
+
+
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+    }
+}
