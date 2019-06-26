f90f2b1fd58ff734c0358a3519c8817e8d26193e
Josef Lintz <yosi1.lints@gmail.com>
2019-06-26 11:45:45 +0200

    * Moved ResourceLocator to it's own file

---
+++ ResourceLocator.cs
@@ -0,0 +1,42 @@
+﻿namespace ResourceLocator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using System.Text;
+
+
+    /// <summary>
+    /// A resource locator that finds embedded resources in an application
+    /// </summary>
+    public class ResourceLocator
+    {
+        private Assembly _currentAssembly;
+
+        /// <summary>
+        /// Default constructor. Creates a default assembly from the currently executing code
+        /// </summary>
+        public ResourceLocator() :
+            this(Assembly.GetExecutingAssembly())
+        {
+        }
+
+
+        public ResourceLocator(Assembly assembly)
+        {
+            _currentAssembly = assembly;
+        }
+
+
+        #region Public functions
+
+        public ManifestResourceInfo FindResource(string resourceName)
+        {
+            ManifestResourceInfo resource = _currentAssembly.GetManifestResourceInfo(resourceName);
+
+            return resource;
+        }
+
+        #endregion
+    }
+}
