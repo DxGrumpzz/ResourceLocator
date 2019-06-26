c0b3ef2a532064c28363d87d8df9a3f4f3665839
Josef Lintz <yosi1.lints@gmail.com>
2019-06-26 13:09:02 +0200

    + Added a function to ResourceInfo that returns the Stream as an array of bytes

--- ResourceLocator/ResourceInfo.cs
+++ ResourceLocator/ResourceInfo.cs
@@ -7,8 +7,38 @@
     /// </summary>
     public class ResourceInfo
     {
+        #region Public properties
+
         public string ResourceName { get; set; }
         public Stream ResourceStream { get; set; }
         public string ResourceFullname { get; set; }
+
+        #endregion
+
+
+
+        #region Public methods
+
+        /// <summary>
+        /// Returns the Resource's data Stream to a byte array
+        /// </summary>
+        /// <returns></returns>
+        public byte[] ToByteArray()
+        {
+            // Validation
+            if (ResourceStream is null)
+                return null;
+
+            // Holds the Resource's data stream 
+            MemoryStream memoryStream = new MemoryStream();
+
+            // Copy resource stream data to the memory stream
+            ResourceStream.CopyTo(memoryStream);
+
+
+            return memoryStream.ToArray();
+        }
+
+        #endregion
     }
 }
